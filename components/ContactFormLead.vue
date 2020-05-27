c650ce4b52f8850fc866dcb533094578a4b7dcd1
unknown <chap6680@gmail.com>
2020-05-27 16:27:20 -0600

    rubin leads

---
+++ components/ContactFormLead.vue
@@ -0,0 +1,88 @@
+<template>
+<form action="https://webto.salesforce.com/servlet/servlet.WebToCase?encoding=UTF-8" method="POST">
+<input type=hidden name="orgid" value="00D5w000007AjKI">
+<input type=hidden name="retURL" value="https://www.rubinbrown.com/article/8334/COVID-19-Updates-to-the-PPP-Loan-Frequently-Asked-Questions.aspx?articlegroup=1059">
+
+    <!-- Name -->
+    <div class="field">
+      <label class="label is-medium">First Name</label>
+      <div class="control has-icons-left">
+        <input class="input is-medium" id="00N5w00000QFkia" maxlength="75" name="00N5w00000QFkia" type = "text" placeholder="John" />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="user" />
+        </span>
+      </div>
+    </div>
+        <div class="field">
+      <label class="label is-medium">Last Name</label>
+      <div class="control has-icons-left">
+        <input class="input is-medium" id="00N5w00000QFkif" maxlength="255" name="00N5w00000QFkif"  placeholder="Smith" />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="user" />
+        </span>
+      </div>
+    </div>
+
+    <!-- Email -->
+    <div class="field">
+      <label class="label is-medium">Email</label>
+      <div class="control has-icons-left">
+        <input
+          class="input is-medium"
+          type="email"
+          name="email"
+          placeholder="you@email.com"
+        />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="envelope" />
+        </span>
+      </div>
+    </div>
+        <!-- Email -->
+    <div class="field">
+      <label class="label is-medium">Phone</label>
+      <div class="control has-icons-left">
+        <input
+          class="input is-medium"
+          type="phone"
+          placeholder="314.290.3400"
+        />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="phone" />
+        </span>
+      </div>
+    </div>
+    <!-- Subject -->
+    <div class="field">
+      <label class="label is-medium">Question</label>
+      <div class="control has-icons-left">
+        <input
+          id="00N5w00000QFkiV" maxlength="255" name="00N5w00000QFkiV"
+          class="input is-medium"
+          type="text"
+          placeholder="Question"
+        />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="briefcase" />
+        </span>
+      </div>
+    </div>
+    <!-- Message -->
+    <div class="field">
+      <label class="label is-medium">Message</label>
+      <div class="control">
+        <textarea
+          class="textarea"
+          name="description"
+          placeholder="Detailed description of your comment, request, etc"
+          rows="8"
+        ></textarea>
+      </div>
+    </div>
+    <div class="control">
+      <button class="button is-primary">
+        Contact Us
+      </button>
+    </div>
+  </form>
+</template>
---
+++ pages/contactCase.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="contact-page" class="page-wrapper contact-page content-page">
+    <site-hero
+      title="Contact Us"
+      subtitle="Web to Lead Example"
+      image="/uploads/rubinCovid.jpg"
+    ></site-hero>
+    <main-section theme="sidebar-right">
+      <template v-slot:default>
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">
+                EFocus
+              </p>
+              <p class="subtitle">
+                Newsletters
+              </p>
+              <figure class="image is-1by1 ">
+                <opti-image
+                  :src="require('~/assets/uploads/rubinmark.jpg').src"
+                  :srcset="
+                    require('~/assets/uploads/rubinmark.jpg').srcSet
+                  "
+                />
+              </figure>
+              <br />
+              <div class="content">
+                <p>
+                  <strong>
+                    05/21/2020
+                  </strong>
+                </p>
+                <p>
+COVID-19: FASB Proposes Delay For Leases And Revenue Recognition Standards For Private Companies And Not-For-Profit Organizations
+                </p>
+              </div>
+            </article>
+          </div>
+          <div class="tile is-parent is-8">
+            <article class="tile is-child box">
+              <contact-form />
+            </article>
+          </div>
+        </div>
+      </template>
+    </main-section>
+  </div>
+</template>
+<script>
+import ContactForm from '~/components/ContactFormLead'
+export default {
+  head() {
+    return {
+      title: `Contact | ${this.$siteConfig.siteName}`
+    }
+  },
+  components: { ContactForm }
+}
+</script>
